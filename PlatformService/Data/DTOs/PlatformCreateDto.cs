90770a58985433e29e7ca21706e8fced415f29bb
Sourav Barua <sourav.shr6@gmail.com>
2021-11-08 23:20:19 -0500

    Dto for writing Platform created.

--- PlatformService/Data/DTOs/PlatformCreateDto.cs
+++ PlatformService/Data/DTOs/PlatformCreateDto.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlatformService.Dto
 {
     public class PlatformCreateDto
     {
-
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Publisher {get; set;}
+        [Required]
+        public decimal Cost { get; set; }
+        public string Installer{get; set;}
     }
 }
