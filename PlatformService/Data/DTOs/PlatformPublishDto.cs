6a491e1bd91eed141109501d02256122b701fa10
Sourav Barua <sourav.shr6@gmail.com>
2021-12-29 15:53:15 -0500

    Mapping for Dto in profile.

--- PlatformService/Data/DTOs/PlatformPublishDto.cs
+++ PlatformService/Data/DTOs/PlatformPublishDto.cs
@@ -2,7 +2,7 @@ namespace PlatformService.Dtos
 {
     public class PlatformPublishDto
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Event { get; set; }
     }
