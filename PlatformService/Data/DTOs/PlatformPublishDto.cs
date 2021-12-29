f43afa839f62a7a791c67535b7ee64f93224e30b
Sourav Barua <sourav.shr6@gmail.com>
2021-12-29 13:55:23 -0500

    Renamed publish message DTO.

---
+++ PlatformService/Data/DTOs/PlatformPublishDto.cs
@@ -0,0 +1,9 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformPublishDto
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Event { get; set; }
+    }
+}
