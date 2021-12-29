ec2b306ba3c68cc90ad7f64ea6bcc87e5a5df73f
Sourav Barua <sourav.shr6@gmail.com>
2021-12-29 15:49:15 -0500

    Dtos for communication with Message Bus

---
+++ CommandsService/Data/DTOs/PlatformPublishDto.cs
@@ -0,0 +1,9 @@
+namespace CommandsService.Dtos
+{
+    public class PlatformPublishDto
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Event { get; set; }
+    }
+}
