0d22753bacebca6b599bd9c459cdb316ed3bc51a
Sourav Barua <sourav.shr6@gmail.com>
2021-12-31 04:40:41 -0500

    Service for Grpc Platform server.

--- PlatformService/Data/Profiles/PlatformsProfile.cs
+++ PlatformService/Data/Profiles/PlatformsProfile.cs
@@ -13,6 +13,10 @@ namespace PlatformService.Profiles
             CreateMap<Platform, PlatformReadDto>(); // Reading Scenario Model to ReadDto
             CreateMap<PlatformCreateDto, Platform>(); // Writing Scenario Create DTO to Model
             CreateMap<PlatformReadDto, PlatformPublishDto>(); //PlatformRead DTO to PlatformPublishedDto for publishing to Message Bus
+            CreateMap<Platform, GrpcPlatformModel>() //Platform to GrpcPlatformModel
+                .ForMember(dest => dest.PlatformId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Publisher));
         }
     }
 }
